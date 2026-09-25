edea6a7c09a1b7185392c266f542e581b2402849
Djapedjape123 <petafipedjaradic@gmail.com>
2026-09-25 23:04:45 +0200

    proba cl

--- app/kursevi/page.tsx
+++ app/kursevi/page.tsx
@@ -90,7 +90,7 @@ export default function KurseviPage() {
           {activeLang === "SR" ? "Online Edukacije" : "Online Courses"}
         </motion.h1>
 
-        {/* Filter Tabovi (Modernije od padajućeg menija) */}
+        
         <motion.div 
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
